43223ae12cac969f351b0ea12e0334c8da8800fd
ADARSH AHER <aheradarsh6@gmail.com>
2026-07-22 21:53:09 +0530

    Time: 756 ms (35.7%), Space: 19.2 MB (70.51%) - LeetHub

--- 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
+++ 1680-concatenation-of-consecutive-binary-numbers/1680-concatenation-of-consecutive-binary-numbers.py
@@ -5,12 +5,10 @@ class Solution:
         bit_length = 0
         
         for i in range(1, n + 1):
-            # If 'i' is a power of 2, its binary representation needs one more bit
             if (i & (i - 1)) == 0:
                 bit_length += 1
             
-            # Shift the current answer left by 'bit_length' spaces 
-            # and use bitwise OR to append 'i' to the empty spaces
+           
             ans = ((ans << bit_length) | i) % MOD
             
         return ans

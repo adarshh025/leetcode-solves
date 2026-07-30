3c03ffbe77dc7aafee2b8579d4cbc55e77b26029
ADARSH AHER <aheradarsh6@gmail.com>
2026-07-30 13:33:18 +0530

    Time: 319 ms (88.09%), Space: 20.4 MB (95.63%) - LeetHub

---
+++ 3614-process-string-with-special-operations-ii/3614-process-string-with-special-operations-ii.py
@@ -0,0 +1,29 @@
+class Solution:
+    def processStr(self, s: str, k: int) -> str:
+        m = 0
+        for c in s:
+            if c == '*':
+                m = max(0, m - 1)
+            elif c == '#':
+                m <<= 1
+            elif c != '%':
+                m += 1
+                
+        if k >= m:
+            return "."
+            
+        for c in reversed(s):
+            if c == '*':
+                m += 1
+            elif c == '#':
+                m >>= 1
+                if k >= m:
+                    k -= m
+            elif c == '%':
+                k = m - 1 - k
+            else:
+                m -= 1
+                if k == m:
+                    return c
+                    
+        return "."
